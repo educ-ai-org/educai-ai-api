18afbe0d53cb1999c4da15f8d6e5d787a30e5a99
ribgu <gustavo.ribeiro@sptech.school>
2024-04-05 21:53:07 -0300

    rm api key

--- src/clients/google-client.ts
+++ src/clients/google-client.ts
@@ -1,7 +1,7 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
 
 const model = new ChatGoogleGenerativeAI({
-    apiKey: 'AIzaSyBUCK_-ige0dSr0aXdTurCbG44ou_tewbA',
+    apiKey: '',
     modelName: 'gemini-pro'
 })
 
