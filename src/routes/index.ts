cad5c3482507d1162d8bdc6b917f3aca2abda0cc
ribgu <gustavo.ribeiro@sptech.school>
2024-04-07 12:15:50 -0300

    Add getTextFromYoutube service and route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
 import express from 'express'
+import getTextFromYoutube from '../services/getTextFromYoutube/youtubeTranscript'
 
 const router = express.Router()
 
 router.get('/health', (_, res) => {
     res.status(200).send('OK')
+})
+
+router.get('/hello', async (_, res) => {
+    const text = await getTextFromYoutube('https://www.youtube.com/watch?v=lAy04mwaL7U')
+    res.status(200).send(text)
 })
 
 export default router
--- src/services/getTextFromYoutube/youtubeTranscript.ts
+++ src/services/getTextFromYoutube/youtubeTranscript.ts
@@ -1,2 +1,8 @@
 import { YoutubeTranscript } from 'youtube-transcript'
 
+async function getTextFromYoutube(videoUrl: string) {
+    const transcript = await YoutubeTranscript.fetchTranscript(videoUrl)
+    return transcript
+}
+
+export default getTextFromYoutube
