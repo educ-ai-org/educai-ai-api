ab26774005cadede2c846e1b0dd651030c838b4e
ribgu <gustavo.ribeiro@sptech.school>
2024-04-09 17:32:22 -0300

    altera service

--- src/services/getTextFromYoutube/youtubeTranscript.ts
+++ src/services/getTextFromYoutube/youtubeTranscript.ts
@@ -1,8 +1,13 @@
-import { YoutubeTranscript } from 'youtube-transcript'
+import { fetchTranscript } from './fetchTranscript'
 
 async function getTextFromYoutube(videoUrl: string) {
-    const transcript = await YoutubeTranscript.fetchTranscript(videoUrl)
-    return transcript
+    let allText = ''
+    const transcript = await fetchTranscript(videoUrl)
+    for (const text of transcript) {
+        allText += text.text + ' '
+    }
+    console.log(allText)
 }
 
+getTextFromYoutube('https://www.youtube.com/watch?v=kx5qKJK_Jbc')
 export default getTextFromYoutube
