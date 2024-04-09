fd6c64ebbc1c314eb62a2e0b4f5aa8de263e7b6d
ribgu <gustavo.ribeiro@sptech.school>
2024-04-09 17:32:52 -0300

    add return

--- src/services/getTextFromYoutube/youtubeTranscript.ts
+++ src/services/getTextFromYoutube/youtubeTranscript.ts
@@ -3,11 +3,12 @@ import { fetchTranscript } from './fetchTranscript'
 async function getTextFromYoutube(videoUrl: string) {
     let allText = ''
     const transcript = await fetchTranscript(videoUrl)
+
     for (const text of transcript) {
         allText += text.text + ' '
     }
-    console.log(allText)
+
+    return allText
 }
 
-getTextFromYoutube('https://www.youtube.com/watch?v=kx5qKJK_Jbc')
 export default getTextFromYoutube
