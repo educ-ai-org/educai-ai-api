3d3ee62260a57301c04abb2c94bd70b166c729c9
Diego <diegosousa241104@gmail.com>
2024-05-28 17:50:51 -0300

    feat: add env no processo de compilacao

--- src/clients/google-client.ts
+++ src/clients/google-client.ts
@@ -1,4 +1,7 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
+import { config } from 'dotenv';
+
+config();
 
 const model = new ChatGoogleGenerativeAI({
     apiKey: process.env.GOOGLE_API_KEY,
