52d9516d296d17a0f62830d68510c2f3bb256f42
Diego <diegosousa241104@gmail.com>
2024-05-31 21:21:06 -0300

    fix: ajuste conflitos

--- src/models/ResourcesUploaded.ts
+++ src/models/ResourcesUploaded.ts
@@ -1,13 +1,6 @@
 export type ResourcesUploaded = {
-<<<<<<< HEAD
-	youtubeLink?: string;
-	document?: Express.Multer.File | null;
-	audio?: Express.Multer.File | null;
-	instructions?: string;
-=======
 	youtubeLink?: string
 	document?: Express.Multer.File | null
 	audio?: Express.Multer.File | null
 	instructions?: string
->>>>>>> main
 }
