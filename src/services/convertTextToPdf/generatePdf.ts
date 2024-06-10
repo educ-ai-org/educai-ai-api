f44a0fc446df9937f869d354822ff5fdf677db7a
ribgu <gustavo.ribeiro@sptech.school>
2024-06-10 19:33:12 -0300

    feat: Update PDF generation to use axios for fetching HTML templates

--- src/services/convertTextToPdf/generatePdf.ts
+++ src/services/convertTextToPdf/generatePdf.ts
@@ -2,15 +2,16 @@ import fs from 'fs'
 import Handlebars from 'handlebars'
 import puppeteer from 'puppeteer'
 import generateMaterial from '../generateMaterial/generateMaterial'
+import axios from 'axios'
 // import path from 'path'
 
 async function generatePDF(data: { content: string }) {
 
 	const iaMaterial = await generateMaterial(data)
 	const newData = { content: iaMaterial }
-	const templateHtml = fs.readFileSync('https://storage.googleapis.com/educai-bucket/ContentTemplate.html', 'utf8')
-	const headerHtml = fs.readFileSync('https://storage.googleapis.com/educai-bucket/HeaderAndFooterTemplate.html', 'utf8')
-	const footerHtml = fs.readFileSync('https://storage.googleapis.com/educai-bucket/HeaderAndFooterTemplate.html', 'utf8')
+	const templateHtml = await axios.get('https://storage.googleapis.com/educai-bucket/ContentTemplate.html').then(response => response.data)
+	const headerHtml = await axios.get('https://storage.googleapis.com/educai-bucket/HeaderAndFooterTemplate.html').then(response => response.data)
+	const footerHtml = await axios.get('https://storage.googleapis.com/educai-bucket/HeaderAndFooterTemplate.html').then(response => response.data)
 
 	const template = Handlebars.compile(templateHtml)
 	const htmlContent = template(newData)
