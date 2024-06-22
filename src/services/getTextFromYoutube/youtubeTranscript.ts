3390d8ef52eab3756163f15db6c5b1aad798e4d1
ribgu <gustavo.ribeiro@sptech.school>
2024-06-22 13:10:39 -0300

    add regex

--- src/services/getTextFromYoutube/youtubeTranscript.ts
+++ src/services/getTextFromYoutube/youtubeTranscript.ts
@@ -1,8 +1,16 @@
 import { fetchTranscript } from './fetchTranscript'
 
-async function getTextFromYoutube(videoUrl: string) {
+async function getTextFromYoutube(videoUrlOrId: string) {
     let allText = ''
-    const transcript = await fetchTranscript(videoUrl)
+    const urlRegex = /v=([a-zA-Z0-9_-]{11})/
+    let id = videoUrlOrId
+
+    const match = videoUrlOrId.match(urlRegex)
+    if (match) {
+        id = match[1]
+    }
+
+    const transcript = await fetchTranscript(id)
 
     for (const text of transcript) {
         allText += text.text + ' '
