0cabaa21c05208c25c193dd90ac45aa0db3e6aa4
ribgu <gustavo.ribeiro@sptech.school>
2024-11-05 19:10:17 -0300

    troca model por gemini pt 2

--- src/services/generateMaterial/generateMaterialGemini.ts
+++ src/services/generateMaterial/generateMaterialGemini.ts
@@ -1,11 +1,11 @@
 import { materialTemplate } from './prompts/materialTemplate'
-import model from '../../clients/google-client'
+import gemini from '../../clients/google-client'
 
 export default async function generateMaterialGemini(
   data: { content: string }
 ): Promise<string> {
   const content = data.content
-  const chain = materialTemplate.pipe(model)
+  const chain = materialTemplate.pipe(gemini)
   const result = await chain.invoke({ content }).then((result) => {
     return result.lc_kwargs.content
   })
